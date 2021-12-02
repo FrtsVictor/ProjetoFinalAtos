c9bb1ba29c2bd2cb1badc3a7ed6143f0e62086e7
FrtsVictor <victorfreitas.jog@gmail.com>
2021-12-01 23:39:08 -0300

    Added base Domain Layer

---
+++ DesafioAtos.Domain/Entities/Customer.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+public class Customer : User
+{
+    public string Name { get; set; }
+    public string Cpf { get; set; }
+    public string Phone { get; set; }
+    public List<Address> Address { get; set; }
+}
