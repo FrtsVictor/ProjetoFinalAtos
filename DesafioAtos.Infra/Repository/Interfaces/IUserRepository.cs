23ee2cf8d2969d5f591b7e09897e457338368c1e
FrtsVictor <victorfreitas.job@gmail.com>
2021-12-07 18:02:36 -0300

    Added UserRepository_

---
+++ DesafioAtos.Infra/Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+using DesafioAtos.Domain.Entities;
+
+namespace DesafioAtos.Infra.Repository.Interfaces;
+
+public interface IUserRepository : IBaseRepository<User>
+{
+    Task<User> GetByUsername(string username);
+}
