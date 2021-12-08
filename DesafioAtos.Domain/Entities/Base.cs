e97d93174b6c1f52defec8a99adc90ec7f6f9130
FrtsVictor <victorfreitas.job@gmail.com>
2021-12-08 12:16:31 -0300

    Removing warnings

--- DesafioAtos.Domain/Entities/Base.cs
+++ DesafioAtos.Domain/Entities/Base.cs
@@ -4,7 +4,7 @@ namespace DesafioAtos.Domain.Entities;
 public abstract class Base
 {
     [JsonIgnore]
-    public long Id { get; set; }
+    public int Id { get; set; }
     public bool Status { get; set; } = true;
     public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
