85ba690c284e888f9d35622333bbae70c7c9fb7c
FrtsVictor <victorfreitas.job@gmail.com>
2021-12-08 22:31:19 -0300

    Added Dtos Coleta, EmpresaColetora, Endereco

--- DesafioAtos.Domain/Dtos/EditEmpresaColetoraDto.cs
+++ DesafioAtos.Domain/Dtos/EditEmpresaColetoraDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace DesafioAtos.Domain.Dtos
 {
-    public class EmpresaColetoraPutDto
+    public class EditEmpresaColetoraDto
     {
         public long Id { get; set; }
         public string Name { get; set; }
---
+++ DesafioAtos.Domain/Dtos/ItemDeColetaDto.cs
@@ -0,0 +1,12 @@
+﻿using DesafioAtos.Domain.Enum;
+
+namespace DesafioAtos.Domain.Dtos
+{
+    public class ItemDeColetaDto
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public CategoriaEnum Categoria { get; set; }
+    }
+}
