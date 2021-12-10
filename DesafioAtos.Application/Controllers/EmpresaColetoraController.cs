3efd2d2410aeb802cb91eaaf724fce47c1ebffcb
waleriamf <waleriamf@gmail.com>
2021-12-09 22:39:52 -0300

    EmpresaColetora

---
+++ DesafioAtos.Application/Controllers/EmpresaColetoraController.cs
@@ -0,0 +1,62 @@
+﻿using DesafioAtos.Domain.Dtos;
+using DesafioAtos.Service.EmpresaColetora;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DesafioAtos.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpresaColetoraController : ControllerBase
+    {
+        private readonly IEmpresaColetoraService _service;
+
+        public EmpresaColetoraController(IEmpresaColetoraService service)
+        {
+            _service = service;
+
+        }
+
+        // GET: api/<ColetaController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var data = await _service.PegaTodasEmpresaColetora();
+            return Ok(new List<EmpresaColetoraDto>());
+        }
+
+        // GET api/<ColetaController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(long id)
+        {
+            var data = await _service.PegaEmpresaColetoraPorId(id);
+            return Ok();
+        }
+
+        // POST api/<ColetaController>
+        //[HttpPost]
+        //public async Task<IActionResult> Post(EmpresaColetoraDto request)
+        //{
+        //    await _service.EmpresaColetoraPost(request);
+        //    return Ok();
+        //}
+
+        // PUT api/<ColetaController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(EditarEmpresaColetoraDto request)
+        {
+
+            await _service.EmpresaColetoraPut(request);
+            return Ok();
+        }
+
+        // DELETE api/<ColetaController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(long id)
+        {
+            await _service.DeletaEmpresaColetora(id);
+            return Ok();
+        }
+    }
+}
---
+++ DesafioAtos.Service/EmpresaColetora/IEmpresaColetoraService.cs
@@ -0,0 +1,13 @@
+﻿using DesafioAtos.Domain.Dtos;
+
+namespace DesafioAtos.Service.EmpresaColetora
+{
+    public interface IEmpresaColetoraService
+    {
+        Task EmpresaColetoraPost(CriarEmpresaColetoraDto request);
+        Task EmpresaColetoraPut(EditarEmpresaColetoraDto request);
+        Task<EmpresaColetoraDto> PegaEmpresaColetoraPorId(long id);
+        Task<List<EmpresaColetoraDto>> PegaTodasEmpresaColetora();
+        Task DeletaEmpresaColetora(long id);
+    }
+}
