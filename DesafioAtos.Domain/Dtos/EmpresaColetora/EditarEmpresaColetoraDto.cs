f5473aacd1c778710237f90c837c7161417a2f11
FrtsVictor <victorfreitas.job@gmail.com>
2021-12-09 22:39:52 -0300

    EmpresaColetora

--- DesafioAtos.Domain/Dtos/EmpresaColetora/EditarEmpresaColetoraDto.cs
+++ DesafioAtos.Domain/Dtos/EmpresaColetora/EditarEmpresaColetoraDto.cs
@@ -1,11 +1,17 @@
-﻿namespace DesafioAtos.Domain.Dtos
+﻿using DesafioAtos.Domain.Entidades;
+
+namespace DesafioAtos.Domain.Dtos
 {
     public class EditarEmpresaColetoraDto
     {
         public long Id { get; set; }
         public string Nome { get; set; }
-        public string Cpf { get; set; }
+        public string Cnpj { get; set; }
         public string Telefone { get; set; }
+        public string Email { get; set; }
         public bool Status { get; set; }
+
+        public List<EnderecoDto> Endereco { get; set; }
+        public List<Categoria> Categoria { get; set; }
     }
 }
---
+++ DesafioAtos.Service/EmpresaColetora/EmpresaColetoraService.cs
@@ -0,0 +1,118 @@
+﻿using AutoMapper;
+using DesafioAtos.Domain.Dtos;
+using DesafioAtos.Infra.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioAtos.Service.EmpresaColetora
+{
+    public class EmpresaColetoraService : IEmpresaColetoraService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public EmpresaColetoraService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+
+        }
+
+        public async Task EmpresaColetoraPost(CriarEmpresaColetoraDto request)
+        {
+            try
+            {
+                var empresaColetora = _mapper.Map<EmpresaColetoraDto>(request);
+
+                var empresaColetoraOrigem = new DesafioAtos.Domain.Entidades.EmpresaColetora
+                {
+                    Nome = request.Nome,
+                    Categoria = request.Categoria,
+                    Email = request.Email,
+                    Cnpj = request.Cnpj,
+                    Telefone = request.Telefone,
+                    
+                };
+
+                await _unitOfWork.EmpresaColetoraRepository.CriarAsync(empresaColetoraOrigem);
+                await _unitOfWork.SalvarAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task EmpresaColetoraPut(EditarEmpresaColetoraDto request)
+        {
+            try
+            {
+                var empresaColetoraOrigem = _mapper.Map<EmpresaColetoraDto>(request);
+
+                var empresaColetoraBanco = await _unitOfWork.EmpresaColetoraRepository.ObterPorIdAsync(empresaColetoraOrigem.Id);
+
+                empresaColetoraBanco.Status = request.Status;
+                empresaColetoraBanco.Telefone = request.Telefone;
+                empresaColetoraBanco.Email = request.Email;
+                empresaColetoraBanco.Cnpj = request.Cnpj;
+                empresaColetoraBanco.Nome = request.Nome;
+
+
+                _unitOfWork.EmpresaColetoraRepository.Atualizar(empresaColetoraBanco);
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task DeletaEmpresaColetora(long id)
+        {
+            try
+            {
+                var empresaColetoraOrigem = await _unitOfWork.EmpresaColetoraRepository.ObterPorIdAsync(id);
+                //_unitOfWork.ColetaRepository.RemoverAsync(empresaColetoraOrigem);
+                await _unitOfWork.SalvarAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<EmpresaColetoraDto> PegaEmpresaColetoraPorId(long id)
+        {
+            try
+            {
+                var empresaColetoraOrigin = await _unitOfWork.EmpresaColetoraRepository.ObterPorIdAsync(id);
+                var data = _mapper.Map<EmpresaColetoraDto>(empresaColetoraOrigin);
+                return data;
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<List<EmpresaColetoraDto>> PegaTodasEmpresaColetora()
+        {
+            try
+            {
+                var empresaColetoraOrigin = await _unitOfWork.EmpresaColetoraRepository.ObterTodosAsync();
+                var ordenarEmpresaColetora = empresaColetoraOrigin.OrderBy(n => n.Nome).ToList();
+                var data = _mapper.Map<List<EmpresaColetoraDto>>(ordenarEmpresaColetora);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
