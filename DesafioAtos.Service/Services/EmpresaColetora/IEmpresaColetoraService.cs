b19e3281956291eb9100354e8bed4e093548bdd5
FrtsVictor <victorfreitas.job@gmail.com>
2021-12-15 07:46:55 -0300

    testeEmpresa

--- DesafioAtos.Service/Services/EmpresaColetora/IEmpresaColetoraService.cs
+++ DesafioAtos.Service/Services/EmpresaColetora/IEmpresaColetoraService.cs
@@ -1,14 +1,18 @@
 ﻿using DesafioAtos.Domain.Dtos;
-using DesafioAtos.Service.Fabrica.Services;
+
 
 namespace DesafioAtos.Service.Services.EmpresaColetora
 {
     public interface IEmpresaColetoraService
     {
-        Task EmpresaColetoraPost(CriarEmpresaColetoraDto request);
-        Task EmpresaColetoraPut(EditarEmpresaColetoraDto request);
-        Task<EmpresaColetoraDto> GetEmpresaColetoraPorId(long id);
-        Task<List<EmpresaColetoraDto>> GetTodasEmpresaColetora();
-        Task DeletaEmpresaColetora(long id);
+
+        Task<DesafioAtos.Domain.Entidades.EmpresaColetora> CriarEmpresaColetora(CriarEmpresaColetoraDto criarEmpresaDto);
+        Task<IEnumerable<string>> ObterEmpresaColetoraId(long idEmpresa);
+        Task<IEnumerable<string>> ObterEmpresaColetora();
+        Task Atualizar(EditarEmpresaColetoraDto atualizarEmpresaDto);
+        Task RemoverEmpresaColetora(long id);
+
+
+
     }
 }
