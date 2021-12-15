c41dffa8dc86859a71a73c96d113da82ce8ca4e2
FrtsVictor <victorfreitas.job@gmail.com>
2021-12-15 18:17:52 -0300

    Added ETipoRepository

--- DesafioAtos.Infra/Repository/EnderecoRepository.cs
+++ DesafioAtos.Infra/Repository/EnderecoRepository.cs
@@ -1,6 +1,7 @@
 ﻿using DesafioAtos.Domain.Entidades;
 using DesafioAtos.Infra.Context;
 using DesafioAtos.Infra.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace DesafioAtos.Infra.Repository
@@ -8,6 +9,9 @@ namespace DesafioAtos.Infra.Repository
     public class EnderecoRepository : BaseRepository<Endereco>, IEnderecoRepository
     {
         public EnderecoRepository(DatabaseContext context, ILogger logger) : base(context, logger) { }
+
+        public async Task<IEnumerable<Endereco?>> ObterTodosPorIdEmpresaAsync(int idEmpresa) =>
+            await dbSet.Where(x => x.IdEmpresaColeta == idEmpresa).ToListAsync();
 
     }
 }
