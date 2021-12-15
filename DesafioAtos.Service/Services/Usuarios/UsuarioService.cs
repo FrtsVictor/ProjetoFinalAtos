7dfd78d7b2b94ac79448fcadca47bc3b27ef3469
FrtsVictor <victorfreitas.job@gmail.com>
2021-12-15 18:10:47 -0300

    Fixed CRUD  methods

--- DesafioAtos.Service/Services/Usuarios/UsuarioService.cs
+++ DesafioAtos.Service/Services/Usuarios/UsuarioService.cs
@@ -1,33 +1,25 @@
-﻿using DesafioAtos.Domain.Core;
-using DesafioAtos.Domain.Dtos;
+﻿using DesafioAtos.Domain.Dtos;
 using DesafioAtos.Domain.Entidades;
 using DesafioAtos.Domain.Enums;
-using DesafioAtos.Domain.Exceptions;
 using DesafioAtos.Domain.Mapper;
 using DesafioAtos.Infra.UnitOfWorks;
-using DesafioAtos.Service.Exceptions;
 using DesafioAtos.Service.Validacoes;
 using Np.Cryptography;
+using DesafioAtos.Service.Services;
 
 namespace DesafioAtos.Service.Usuarios
 {
-    public class UsuarioService : IUsuarioService
+    public class UsuarioService : BaseService, IUsuarioService
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
-        private readonly string _chaveParaCriptografia;
-        private readonly ICriptografo _criptografo;
 
         public UsuarioService(
             IUnitOfWork unitOfWork,
-            IMapper mapper,
-            ICriptografo criptografo,
-            string chaveParaCriptografia)
+            IMapper mapper)
         {
-            this._chaveParaCriptografia = chaveParaCriptografia;
             this._unitOfWork = unitOfWork;
             this._mapper = mapper;
-            this._criptografo = criptografo;
         }
 
         public async Task<Usuario> CriarConta(CriarUsuarioDto criarUsuarioDto)
@@ -38,44 +30,20 @@ namespace DesafioAtos.Service.Usuarios
                 throw new InvalidOperationException(string.Join("\n", resultadoValidacao.Errors.Select(s => s)));
 
             var usuarioParaCriacao = _mapper.MapCriarUsuarioDtoToUsuario(criarUsuarioDto);
-
-
-            var senhaCriptografada = _criptografo.Criptografar(_chaveParaCriptografia, usuarioParaCriacao.Senha);
-            usuarioParaCriacao.Senha = senhaCriptografada;
-
             return await _unitOfWork.ExecutarAsync(async () =>
                  await _unitOfWork.Users.CriarAsync(usuarioParaCriacao));
         }
 
-        public async Task Atualizar(AtualizarUsuarioDto atualizarUsuarioDto)
+        public async Task Atualizar(int idUsusario, EditarUsuarioDto editarUsuarioDto)
         {
-            await _unitOfWork.VoidExecutarAsync(async () =>
-            {
-                var usuarioParaAtualizar = await _unitOfWork.Users.ObterPorIdAsync(atualizarUsuarioDto.Id);
-                ValidarEntidade(usuarioParaAtualizar == null, "Falha ao encontrar usuario, verificar token");
-                string senhaParaAtualizar = atualizarUsuarioDto.Senha;
-
-                usuarioParaAtualizar.Login = atualizarUsuarioDto.Login ?? usuarioParaAtualizar.Login;
-
-                usuarioParaAtualizar.Senha = string.IsNullOrEmpty(senhaParaAtualizar)
-                    ? usuarioParaAtualizar.Senha
-                    : _criptografo.Criptografar(_chaveParaCriptografia, senhaParaAtualizar);
-
-                usuarioParaAtualizar.Nome = atualizarUsuarioDto.Nome ?? usuarioParaAtualizar.Nome;
-
-                _unitOfWork.Users.Atualizar(usuarioParaAtualizar);
-                return usuarioParaAtualizar;
-            });
+            var usuarioParaAtualizar = await _unitOfWork.Users.ObterPorIdAsync(idUsusario);
+            ValidarEntidade(usuarioParaAtualizar == null, "Falha ao encontrar usuario, verificar token");
+            _mapper.MapEditarUsuarioDtoToUsuario(editarUsuarioDto, usuarioParaAtualizar);
+            _unitOfWork.Executar(() => _unitOfWork.Users.Atualizar(usuarioParaAtualizar));
         }
 
-        public async Task Remover(long id)
-        {
-            await _unitOfWork.ExecutarAsync(async () =>
-           {
-               await _unitOfWork.Users.RemoverAsync(id);
-               return id;
-           });
-        }
+        public async Task Remover(int id) => await _unitOfWork.VoidExecutarAsync(
+            async () => await _unitOfWork.Users.RemoverAsync(id));
 
         public async Task<ECategoria> AdicionarCategoria(CategoriaDto adicionarCategoriaDto)
         {
@@ -84,8 +52,7 @@ namespace DesafioAtos.Service.Usuarios
             ValidarCategoria(idCategoria);
             var categoriaExistente = await _unitOfWork.CategoriaUsuario.ObterCategoriaPorId(idCategoria, idUsuario);
             ValidarEntidade(categoriaExistente != null, "Categoria já cadastrada");
-
-            var categoriaUsuario = new CategoriaUsuario() { IdCategoria = idCategoria, IdUsuario = idUsuario };
+            var categoriaUsuario = _mapper.CriarCategoriaUsuario(idUsuario, idCategoria);
 
             return await _unitOfWork.ExecutarAsync(async () =>
             {
@@ -108,41 +75,24 @@ namespace DesafioAtos.Service.Usuarios
                 {
                     await _unitOfWork.CategoriaUsuario.RemoverAsync(categoriaExistente.Id);
                 }
-
-                return categoriaExistente;
             });
         }
 
-        public async Task<IEnumerable<string>> ObterCategorias(int idUsuario)
+        public async Task<IEnumerable<string>?> ObterCategorias(int idUsuario)
         {
             var categorias = await _unitOfWork.CategoriaUsuario.ObterTodasCategoriasPorUsuario(idUsuario);
             return categorias?.Select(x => x.Nome);
         }
 
 
-        public async Task<IEnumerable<CategoriaEmpresa>> ObterEmpresasPorCategoriaUsuario(int idUsuario) => await _unitOfWork
+        public async Task<IEnumerable<EmpresaColetoraDto>?> ObterEmpresasPorCategoriaUsuario(int idUsuario) => await _unitOfWork
             .ExecutarAsync(async () =>
             {
                 var categorias = await _unitOfWork.CategoriaUsuario.ObterTodasCategoriasPorUsuario(idUsuario);
                 var ids = categorias?.Select(x => x.Id);
                 var empresas = await _unitOfWork.CategoriaEmpresa.ObterEmpresasPorIdCategoria(ids);
-
-                return empresas.DistinctBy(x => x.IdEmpresaColetora);
+                empresas = empresas?.DistinctBy(x => x.IdEmpresaColetora);
+                return empresas?.Select(x => _mapper.MapEmpresaColetoraToEmpresaColetoraDto(x.EmpresaColetora));
             });
-
-
-        private void ValidarCategoria(int value)
-        {
-            if (!Enum.IsDefined(typeof(ECategoria), value))
-                throw new InvalidEnumException();
-        }
-
-        private void ValidarEntidade(bool isTrue, string mensagemErro)
-        {
-            if (isTrue)
-            {
-                throw new BadRequestException(mensagemErro);
-            }
-        }
     }
 }
