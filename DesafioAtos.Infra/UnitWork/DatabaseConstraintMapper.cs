3e494dba7390a0a53eb8d4d45fee00cd0c700a79
FrtsVictor <victorfreitas.job@gmail.com>
2021-12-15 18:17:41 -0300

    Added contraints mappings for telefone, numero

---
+++ DesafioAtos.Infra/UnitWork/DatabaseConstraintMapper.cs
@@ -0,0 +1,38 @@
+using DesafioAtos.Infra.Exceptions;
+using Microsoft.Extensions.Logging;
+
+namespace DesafioAtos.Infra.UnitWork
+{
+    public class DatabaseConstraintMapper : IDatabaseConstraintMapper
+    {
+        private readonly ILogger<DatabaseConstraintMapper> _logger;
+        public DatabaseConstraintMapper(ILogger<DatabaseConstraintMapper> logger)
+        {
+            this._logger = logger;
+        }
+
+        private static readonly Dictionary<string, string> ConstraintKeyAndErrorMessage = new Dictionary<string, string>()
+        {
+            {"IX_Users_Username", "Usuario existente, tente outro login."},
+            {"'Username'. Truncated value:", "Verifique o login."},
+            {"'telefone'. Truncated value:", "Verifique o numero de telefone."},
+            {"UNIQUE KEY constraint 'UQ__Empresa___2A16D97F4BD0B9D7'", "Telefone ja cadastrado."},
+            {"'numero'. Truncated value:", "Verifique o numero do endereco."}
+
+        };
+
+        public void Map(Exception ex)
+        {
+            string errorMessage = ex.InnerException?.Message ?? ex.Message;
+            _logger.LogError(errorMessage);
+
+            foreach (KeyValuePair<string, string> pair in ConstraintKeyAndErrorMessage)
+            {
+                if (errorMessage.ToLower().Contains(pair.Key.ToLower()))
+                {
+                    throw new DataBaseConstraintException(pair.Value, ex);
+                }
+            }
+        }
+    }
+}
