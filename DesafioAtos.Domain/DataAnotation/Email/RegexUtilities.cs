04b6bf3546b1ebde5a7d493e108f52ef27b78b75
FrtsVictor <victorfreitas.job@gmail.com>
2021-12-18 10:02:22 -0300

    Created custom data anotation for cnpj and email

---
+++ DesafioAtos.Domain/DataAnotation/Email/RegexUtilities.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace DesafioAtos.Domain.DataAnotation
+{
+    public class RegexUtilities
+    {
+        public static bool isEmailValido(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper, RegexOptions.None,
+                    TimeSpan.FromMilliseconds(200));
+
+                string DomainMapper(Match match)
+                {
+                    var idn = new IdnMapping();
+                    var domainName = idn.GetAscii(match.Groups[2].Value);
+                    return match.Groups[1].Value + domainName;
+                }
+
+                return Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$", RegexOptions.IgnoreCase,
+                    TimeSpan.FromMilliseconds(250));
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static string RemoveSpecialCharacters(string? text, bool allowSpace = false)
+        {
+            return System.Text.RegularExpressions.Regex.Replace(text ?? "", allowSpace
+                ? @"[^0-9a-zA-ZéúíóáÉÚÍÓÁèùìòàÈÙÌÒÀõãñÕÃÑêûîôâÊÛÎÔÂëÿüïöäËYÜÏÖÄçÇ\s]+?"
+                : @"[^0-9a-zA-ZéúíóáÉÚÍÓÁèùìòàÈÙÌÒÀõãñÕÃÑêûîôâÊÛÎÔÂëÿüïöäËYÜÏÖÄçÇ]+?", string.Empty);
+        }
+    }
+}
--- DesafioAtos.Domain/Dtos/EmpresaColetora/EditarEmpresaColetoraDto.cs
+++ DesafioAtos.Domain/Dtos/EmpresaColetora/EditarEmpresaColetoraDto.cs
@@ -1,25 +1,23 @@
 using System.ComponentModel.DataAnnotations;
+using DesafioAtos.Domain.DataAnotation;
+using DesafioAtos.Domain.DataAnotation.Email;
 
 namespace DesafioAtos.Domain.Dtos
 {
     public class EditarEmpresaColetoraDto
     {
-        [Required(ErrorMessage = "Propriedade {0} é obrigatória.")]
         [StringLength(maximumLength: 200, MinimumLength = 4, ErrorMessage = "A propriedade {0}  deve conter entre {1} e {2} caracteres")]
         public string? Nome { get; set; }
 
-        [Required(ErrorMessage = "Propriedade {0} é obrigatória.")]
-        [StringLength(maximumLength: 18, MinimumLength = 14, ErrorMessage = "A propriedade {0}  deve conter entre {1} e {2} caracteres")]
+        [ValidacaoCnpj]
         public string? Cnpj { get; set; }
 
-        [Required]
         [Phone]
         [StringLength(maximumLength: 14, MinimumLength = 9, ErrorMessage = "A propriedade {0}  deve conter entre {1} e {2} caracteres")]
         public string? Telefone { get; set; }
 
 
-        [Required(ErrorMessage = "Informe o seu email")]
-        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Informe um email válido...")]
+        [ValidacaoEmail]
         public string? Email { get; set; }
 
         [Required]
